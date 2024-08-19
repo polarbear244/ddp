1134f8aaa77cdfbceb5edadb41f28e2e35c89d2f
polarbear244 <sarah_koesters@yahoo.de>
2024-08-19 21:18:33 +0100

    Create ui.r

---
+++ myapp/ui.r
@@ -0,0 +1,13 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Simple Shiny App"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
+    ),
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
